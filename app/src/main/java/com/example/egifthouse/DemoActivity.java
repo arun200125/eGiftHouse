5204d307fa253ab1299a27e9889b4fdb4f3833ec
arun200125 <arunmacherla825@gmail.com>
2022-08-13 14:23:19 +0530

    Initial

---
+++ app/src/main/java/com/example/egifthouse/DemoActivity.java
@@ -0,0 +1,21 @@
+package com.example.egifthouse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class DemoActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+
+
+    }
+}
